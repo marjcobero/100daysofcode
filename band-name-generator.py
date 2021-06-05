1d053b05777ea9930e88822b6d4723c099ea94a2
mcobero <69501884+mcobero@users.noreply.github.com>
2021-06-04 23:07:40 -0700

    day one: a name generator for band names

---
+++ band-name-generator.py
@@ -0,0 +1,9 @@
+#Welcomes the user
+print("Welcome to the Band Name Generator.")
+#Lets the user responds the question
+#The city the user grew up in
+street = input("What's name of the city you grew up in?\n")
+#The user's pet name
+pet = input("What's your pet's name?\n")
+#The result of the generator based on the data
+print("Your band name could be " + street + " " + pet)
