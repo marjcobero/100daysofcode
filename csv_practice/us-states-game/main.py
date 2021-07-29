9929559c9f975094b935127bb29d6a0798a7605f
mcobero <69501884+mcobero@users.noreply.github.com>
2021-07-28 22:51:29 -0700

    shorten the for loop and changed it to a list comprehension

--- csv_practice/us-states-game/main.py
+++ csv_practice/us-states-game/main.py
@@ -21,10 +21,7 @@ while len(guessed_states) < 50:
     print(answer_state)
 
     if answer_state == "Exit": # if exit is typed it will exit the game
-        missing_states = []
-        for state in all_states:
-            if state not in guessed_states:
-                missing_states.append(state)
+        missing_states = [state for state in all_states if state not in guessed_states]
         new_data = pandas.DataFrame(missing_states)
         new_data.to_csv("states_to_learn.csv")
         break
